48171d55c57abd89303f30e444c845f5f084df87
jvacaris42 <84627746+JorgeVB20007@users.noreply.github.com>
2023-04-15 19:01:07 +0200

    jvacaris - Added basic responses (#9)
    * First parsing functions, specifically error handling
    
    * basics response format
    
    ---------
    
    Co-authored-by: jvacaris <>

--- src/parsing/request_header_parsing.hpp
+++ src/parsing/request_header_parsing.hpp
@@ -6,7 +6,7 @@
 /*   By: jvacaris <jvacaris@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/06 20:31:06 by jvacaris          #+#    #+#             */
-/*   Updated: 2023/04/09 21:18:03 by jvacaris         ###   ########.fr       */
+/*   Updated: 2023/04/11 20:14:27 by jvacaris         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,6 @@
 # define REQUEST_HEADER_PARSING_HPP
 
 #include "../general.hpp"
-
-
-
-struct s_request_info
-{
-	short method;
-	std::string path;
-	std::string http_version;
-};
 
 
 void header_parser(std::string fullrequest, struct s_request_info &header_struct, std::map<std::string, std::string> &header_map, std::string &body);
