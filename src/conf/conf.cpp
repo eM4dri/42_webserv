85ab45beae6288e3b206ba29f22af369c7ce3c91
eM4dri <edu.mail.delivery+github@gmail.com>
2023-04-30 23:32:41 +0200

    server parameters constructor from conf file

--- src/conf/conf.cpp
+++ src/conf/conf.cpp
@@ -6,7 +6,7 @@
 /*   By: emadriga <emadriga@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/15 17:32:30 by emadriga          #+#    #+#             */
-/*   Updated: 2023/04/21 23:30:28 by emadriga         ###   ########.fr       */
+/*   Updated: 2023/04/30 23:24:46 by emadriga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,14 +19,17 @@
 #include <iostream>		// std::out, std::endl
 #include <locale>		// std::locale, std::isspace
 #include <cstdlib>		// std::exit
-#include <cstring>		//	std::strcpy
+#include <cstring>		// std::strcpy
+#include <limits.h>		// UCHAR_MAX, USHRT_MAX
 
 #define ERROR_OPENING_FILE "Error opening file"
 #define ISSPACE_CHARACTERS " \t\n\v\f\r"
 #define NUMBER_CHARACTERS "0123456789"
-#define INVALID_CONF_FILE_END "Invalid configuration file, not allowed ending directory"
-#define INVALID_CONF_FILE_BRACES "Invalid configuration file, curly braces not closed"
-#define INVALID_CONF_UNKOWN_DIRECTIVE "Invalid configuration file, unknown key directive"
+#define ERROR_CONF(ERROR) LOG_ERROR("Invalid configuration file, " ERROR)
+#define INVALID_FILE_END "not allowed ending directory"
+#define INVALID_FILE_BRACES "curly braces not closed"
+#define INVALID_UNKOWN_DIRECTIVE "unknown key directive"
+#define INVALID_LISTEN "only accepts IPv4 (BYTE.BYTE.BYTE.BYTE:PORT & PORT)"
 
 enum e_curly_braces_level{
 	CLOSED,
@@ -41,36 +44,19 @@ namespace ft
 conf::conf( const char* filename )
 {
 	std::ifstream ifs;
-	// serverc *sc;
-
 	ifs.open (filename, std::ifstream::in);
-	// std::ifstream ifs (filename);
-	std::string	line;
 
 	if (ifs.is_open() == false)
 	{
-	 	LOG_ERROR(ERROR_OPENING_FILE);
+		LOG_ERROR(ERROR_OPENING_FILE);
 		std::exit(1);
 	}
-	while (std::getline(ifs, line))
-		_processLine(line);
-	_validate_conf();
-	_print_processed_conf();
+
+	_process_conf_file(ifs);
+	// _print_processed_conf();
+	_validate_processed_conf();
 	_load_configuration();
-	LOG("HOLA");
-	LOG(servers.size());
-	for (std::vector<serverconf>::iterator it=servers.begin(); it!=servers.end(); ++it){
-		LOG(it->address <<":"<< it->port );
-		for (std::map<std::string, location>::iterator it2=it->locations.begin(); it2!=it->locations.end(); ++it2){
-			LOG(it2->first);
-			LOG("\t " << it2->second.autoindex);
-			LOG("\t " << it2->second.client_max_body_size);
-			LOG("\t " << it2->second.index);
-			LOG("\t " << it2->second.methods);
-			LOG("\t " << it2->second.redirect);
-			LOG("\t " << it2->second.path);
-		}
-	}
+	// _print_loaded_conf();
 
 }
 
@@ -87,13 +73,13 @@ conf::~conf()
 		_accepted_methods.clear();
 }
 
-void conf::_parse_path(std::string root, location *location)
+void conf::_parse_path(const std::string &root, location *location)
 {
 	//TODO validate format
 	location->path = root.substr(0, root.find_first_of(ISSPACE_CHARACTERS));
 }
 
-void conf::_parse_methods(std::string methods, location *location)
+void conf::_parse_methods(const std::string &methods, location *location)
 {
 	//TODO validate format
 	location->methods = 0;
@@ -114,51 +100,112 @@ void conf::_parse_methods(std::string methods, location *location)
 		}
 	}
 }
-void conf::_parse_autoindex(std::string autoindex, location *location)
+void conf::_parse_autoindex(const std::string &autoindex, location *location)
 {
 	//TODO validate format
 	location->autoindex = (autoindex == "on");
 }
-void conf::_parse_client_max_body_size(std::string client_max_body_size, location *location)
+void conf::_parse_client_max_body_size(const std::string &client_max_body_size, location *location)
 {
 	//TODO validate format
 	location->client_max_body_size = std::atoi(client_max_body_size.c_str());
 }
-void conf::_parse_index(std::string index, location *location)
+void conf::_parse_index(const std::string &index, location *location)
 {
 	//TODO validate format
 	location->index = index;
 }
-void conf::_parse_redirect(std::string redirect, location *location)
+void conf::_parse_redirect(const std::string &redirect, location *location)
 {
 	//TODO validate format
 	location->redirect = redirect;
 }
 
-void conf::_parse_location_directive(std::pair <std::string,std::string> conf, location *location)
+void conf::_parse_location_directive(const std::pair <std::string,std::string> &directive, location *location)
 {
-	if (conf.first == "root")
-		_parse_path(conf.second, location);
-	else if (conf.first == "index")
-		_parse_index(conf.second, location);
-	else if (conf.first == "client_max_body_size")
-		_parse_client_max_body_size(conf.second, location);
-	else if (conf.first == "autoindex")
-		_parse_autoindex(conf.second, location);
-	else if (conf.first == "redirect")
-		_parse_redirect(conf.second, location);
-	else if (conf.first == "methods")
-		_parse_methods(conf.second, location);
+	if (directive.first == "location")
+		_parse_path(directive.second, location);
+	else if (directive.first == "index")
+		_parse_index(directive.second, location);
+	else if (directive.first == "client_max_body_size")
+		_parse_client_max_body_size(directive.second, location);
+	else if (directive.first == "autoindex")
+		_parse_autoindex(directive.second, location);
+	else if (directive.first == "redirect")
+		_parse_redirect(directive.second, location);
+	else if (directive.first == "methods")
+		_parse_methods(directive.second, location);
 }
 
-unsigned short _setPort(std::string str_port)
+
+bool _valid_byte(const std::string &byte)
+{
+	size_t len = byte.length();
+
+	if (len > 3)
+		return false;
+	for (size_t i = 0; i < len; i++)
+	{
+		if(!std::isdigit(byte[i]))
+			return false;
+	}
+	if (std::atoi(byte.c_str()) > UCHAR_MAX)
+		return false;
+	return true;
+}
+
+bool _valid_port(const std::string &port)
+{
+	size_t len = port.length();
+
+	if (len > 5)
+		return false;
+	for (size_t i = 0; i < len; i++)
+	{
+		if(!std::isdigit(port[i]))
+			return false;
+	}
+	if (std::atoi(port.c_str()) > USHRT_MAX)
+		return false;
+	return true;
+}
+
+bool _validate_listen(const std::string &listen)
+{
+	if (listen.find_first_of(".") != std::string::npos)
+	{
+		std::stringstream ss(listen);
+		std::string byte;
+		std::getline(ss, byte, '.'); // X.0.0.0:00
+		if (!_valid_byte(byte))
+			return false;
+		std::getline(ss, byte, '.'); // 0.X.0.0:00
+		if (!_valid_byte(byte))
+			return false;
+		std::getline(ss, byte, '.'); // 0.0.X.0:00
+		if (!_valid_byte(byte))
+			return false;
+		std::getline(ss, byte, ':'); // 0.0.0.X:00
+		if (!_valid_byte(byte))
+			return false;
+		std::getline(ss, byte, ';'); // 0.0.0.0:XX
+		if (!_valid_port(byte))
+			return false;
+	}
+	else if (!_valid_port(listen))
+		return false;
+	return true;
+}
+
+unsigned short _setPort(const std::string &str_port)
 {
 	return (std::atoi(str_port.c_str()));
 }
 
-void conf::_parse_listen(std::string listen, serverconf *server)
+void conf::_parse_listen(const std::string &listen, serverconf *server)
 {
-	//TODO validate format
+	if (!_validate_listen(listen))
+		ERROR_CONF(INVALID_LISTEN);
 	size_t port_start = listen.find_first_of(":");
 	if (port_start == std::string::npos)
 		server->port = std::atoi(listen.c_str());
@@ -169,18 +216,18 @@ void conf::_parse_listen(std::string listen, serverconf *server)
 	}
 }
 
-void conf::_parse_root(std::string root, serverconf *server)
+void conf::_parse_root(const std::string &root, serverconf *server)
 {
 	//TODO validate format
 	server->root = root;
 }
 
-void conf::_parse_server_directive(std::pair<std::string,std::string> conf, serverconf *server)
+void conf::_parse_server_directive(const std::pair<std::string,std::string> &directive, serverconf *server)
 {
-	if (conf.first == "listen")
-		_parse_listen(conf.second, server);
-	if (conf.first == "root")
-		_parse_root(conf.second, server);
+	if (directive.first == "listen")
+		_parse_listen(directive.second, server);
+	if (directive.first == "root")
+		_parse_root(directive.second, server);
 }
 
 void _set_server_defaults(serverconf *server)
@@ -207,12 +254,12 @@ void conf::_load_configuration()
 	serverconf server;
 	location location;
 	_load_acepted_methods();
-	for (std::vector<std::pair<std::string,std::string> >::iterator it=_conf.begin(); it!=_conf.end(); ++it){
+	for (std::vector< std::pair<std::string,std::string> >::iterator it=_conf.begin(); it!=_conf.end(); ++it){
 		if (it->first == "}")
 		{
 			curly_braces_level--;
 			if (curly_braces_level == CLOSED)
-				servers.push_back(server);
+				this->servers.push_back(server);
 			else if (curly_braces_level == OPEN_SERVER)
 				server.locations.insert(std::make_pair(location.path, location));
 		}
@@ -222,12 +269,32 @@ void conf::_load_configuration()
 			if (curly_braces_level == OPEN_SERVER)
 				_set_server_defaults(&server);
 			else if (curly_braces_level == OPEN_LOCATION)
+			{
 				_set_location_defaults(&location);
+
+			}
 		}
 		if (curly_braces_level == OPEN_SERVER)
 			_parse_server_directive(*it, &server);
 		else if (curly_braces_level == OPEN_LOCATION)
 			_parse_location_directive(*it, &location);
+	}
+}
+
+void conf::print_loaded_conf()
+{
+	LOG(this->servers.size() << " server configurations loaded");
+	for (std::vector<serverconf>::iterator it= this->servers.begin(); it!= this->servers.end(); ++it){
+		LOG("Listen\t"<<it->address <<":"<< it->port );
+		for (std::map<std::string, location>::iterator it2=it->locations.begin(); it2!=it->locations.end(); ++it2){
+			LOG(it2->first);
+			LOG("autoindex\t " << it2->second.autoindex);
+			LOG("client_max_body_size\t " << it2->second.client_max_body_size);
+			LOG("index\t " << it2->second.index);
+			LOG("methods\t " << it2->second.methods);
+			LOG("redirect\t " << it2->second.redirect);
+			LOG("path\t " << it2->second.path);
+		}
 	}
 }
 
@@ -267,7 +334,7 @@ void conf::_load_acepted_methods()
 	_accepted_methods.push_back("POST");
 }
 
-void conf::_validate_conf()
+void conf::_validate_processed_conf()
 {
 	_load_valid_conf_keys();
 	int	curly_braces_count;
@@ -277,7 +344,7 @@ void conf::_validate_conf()
 		second_back = (it->second.length() == 0) ? '\0' : it->second[it->second.length() - 1];
 		if (it->first != "}" && second_back != '{' && second_back != ';')
 			// Every directive ends with semicolon or cruly braces (simple/block directive)
-			LOG_ERROR(INVALID_CONF_FILE_END);
+			ERROR_CONF(INVALID_FILE_END);
 		else
 		{
 			if (it->first == "}")
@@ -286,21 +353,21 @@ void conf::_validate_conf()
 				curly_braces_count++;
 			if( _valid_conf_keys.find(it->first) == _valid_conf_keys.end() )
 				// Unkown key directive
-				LOG_ERROR(INVALID_CONF_UNKOWN_DIRECTIVE);
+				ERROR_CONF(INVALID_UNKOWN_DIRECTIVE);
 		}
 	}
 	if (curly_braces_count != 0)
 		// Curly braces must have start & end
-		LOG_ERROR(INVALID_CONF_FILE_BRACES);
+		ERROR_CONF(INVALID_FILE_BRACES);
 }
 
-void conf::_processLine(std::string line)
+void conf::_process_conf_line(std::string &line)
 {
 	size_t	key_start = 0;
 	while (std::isspace(line[key_start], std::locale()))
 			key_start++;
 	if (key_start > 0)
-		line.erase(0,key_start);
+		line.erase(0, key_start);
 	size_t comment_start = line.find_first_of('#', 0);
 	if (comment_start != std::string::npos)	//remove comments
 	{
@@ -321,10 +388,16 @@ void conf::_processLine(std::string line)
 			std::getline(ss, key, line[key_end]);
 			std::string value;
 			std::getline(ss, value, '\0');
-			value.erase(0, value.find_first_not_of(ISSPACE_CHARACTERS,0)); //rtrim isspace
+			value.erase(0, value.find_first_not_of(ISSPACE_CHARACTERS,0)); //ltrim isspace
 			_conf.push_back(std::pair<std::string,std::string>(key,value));
 		}
 	}
 }
 
+void conf::_process_conf_file(std::ifstream &ifs)
+{
+	std::string	line;
+	while (std::getline(ifs, line))
+		_process_conf_line(line);
+}
 }	// Nammespace ft
