6c7dcabe2221b99f88dc6e4a8c702f792588dd59
Eduardo Madrigal martinez <emadriga@c1r17s2.42madrid.com>
2023-05-09 21:13:30 +0200

    conf cgi & file_root

--- TODO.md
+++ TODO.md
@@ -5,3 +5,9 @@
 - [ ] Formats unifing passing (`const &`, private var and methods,...)
 > About `const &` https://42born2code.slack.com/archives/CNA2Z8RBP/p1680868478345079
 - [ ] Validate multiple locations on same level on loading `.conf` example `location	/file /hola	`
+- [ ] Parse index on `conf` default page
+- [ ] Parse `conf` configure root in every location, apeeding server root
+- [ ] Create default `/` location
+- [ ] Cgi
+- [ ] Apply `correct_path()` on `conf` default_root, file_root & request_path.
+- [ ] fix errors `example.conf` look for kapouet
--- src/conf/conf.hpp
+++ src/conf/conf.hpp
@@ -6,7 +6,7 @@
 /*   By: emadriga <emadriga@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/15 17:32:27 by emadriga          #+#    #+#             */
-/*   Updated: 2023/05/08 19:23:17 by emadriga         ###   ########.fr       */
+/*   Updated: 2023/05/09 20:36:58 by emadriga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -64,17 +64,20 @@ class conf{
 		//	parse server directives
 		void _parse_server_directive(const std::pair <std::string,std::string> &directive, serverconf *server);
 		void _parse_listen(const std::string &listen, serverconf *server);
-		void _parse_root(const std::string &root, serverconf *server);
+		void _parse_default_root(const std::string &default_root, serverconf *server);
 
 		//	parse location directives
 		void _parse_location_directive(const std::pair <std::string,std::string> &directive, location *location);
-		void _parse_path(const std::string &path, location *location);
+		void _parse_request_path(const std::string &path, location *location);
 		void _parse_methods(const std::string &methods, location *location);
 		void _parse_autoindex(const std::string &autoindex, location *location);
 		void _parse_client_max_body_size(const std::string &client_max_body_size, location *location);
 		void _parse_index(const std::string &index, location *location);
 		void _parse_redirect(const std::string &redirect, location *location);
+		void _parse_file_root(const std::string &file_root, location *location);
+		void _parse_cgi(const std::string &cgi, location *location);
 
+		void push_back_server(serverconf &server);
 	private:
 		std::vector<std::string>							_accepted_methods;	// GET, POST, DELETE for validations
 		std::vector<std::pair <std::string,std::string> >	_conf;	// parsed conf for validations
