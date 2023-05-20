a747ba54cac6a490f5944ce0d1c50e3579056633
emadriga <emadriga@student.42madrid.com>
2023-05-20 16:37:50 +0200

    fix script github actions

--- src/conf/disableExceptions.sh
+++ src/conf/disableExceptions.sh
@@ -10,9 +10,9 @@ if [ $(uname -s) = 'Darwin' ]; then
 elif [ $(uname -s) = 'Linux' ]; then
 	while cat ./src/conf/conf.cpp | grep '// LOG_ERROR_CONF(' &> /dev/null;
 	do
-		find ./src/conf -type f -name "conf.cpp" -exec sed -i "" "s|// LOG_ERROR_CONF(|LOG_ERROR_CONF(|" {} +
+		find ./src/conf -type f -name "conf.cpp" -exec sed -i 's|// LOG_ERROR_CONF(|LOG_ERROR_CONF(|' {} +
 	done
-		find ./src/conf -type f -name "conf.cpp" -exec sed -i "" "s|throw std::invalid_argument|// throw std::invalid_argument|" {} +
+		find ./src/conf -type f -name "conf.cpp" -exec sed -i 's|throw std::invalid_argument|// throw std::invalid_argument|' {} +
 else
 	echo "This script is prepared to run either on Linux or Darwin systems"
 fi
--- src/conf/enableExceptions.sh
+++ src/conf/enableExceptions.sh
@@ -11,10 +11,10 @@ done
 elif [ $(uname -s) = 'Linux' ]; then
 while cat ./src/conf/conf.cpp | grep '// throw std::invalid_argument' &> /dev/null;
 do
-	find ./src/conf -type f -name "conf.cpp" -exec sed -i "" "s|// throw std::invalid_argument|throw std::invalid_argument|" {} +
+	find ./src/conf -type f -name "conf.cpp" -exec sed -i 's|// throw std::invalid_argument|throw std::invalid_argument|' {} +
 done
-	find ./src/conf -type f -name "conf.cpp" -exec sed -i "" "s|LOG_ERROR_CONF(|// LOG_ERROR_CONF(|" {} +
-	find ./src/conf -type f -name "conf.cpp" -exec sed -i "" "s|#define // LOG_ERROR_CONF|#define LOG_ERROR_CONF|" {} +
+	find ./src/conf -type f -name "conf.cpp" -exec sed -i 's|LOG_ERROR_CONF(|// LOG_ERROR_CONF(|' {} +
+	find ./src/conf -type f -name "conf.cpp" -exec sed -i 's|#define // LOG_ERROR_CONF|#define LOG_ERROR_CONF|' {} +
 else
 	echo "This script is prepared to run either on Linux or Darwin systems"
 fi
